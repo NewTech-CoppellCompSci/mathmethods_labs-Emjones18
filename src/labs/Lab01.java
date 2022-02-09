bb7cd6c0176cb9e52e24be7577d3f792d8bcab8f
Emjones18 <89420463+Emjones18@users.noreply.github.com>
2022-02-09 15:39:14 -0600

    Update Lab01.java

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -1,14 +1,14 @@
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab01 {
 
 	public static void main(String[] args) {
 		
-		problem01();
-		problem02();
-		problem03();
+		//problem01();
+		//problem02();
+		//problem03();
 		problem04();
 		
 	}
@@ -17,7 +17,17 @@ public class Lab01 {
 	
 	public static void problem01() {
 		
+		Scanner inKey = new Scanner(System.in);
 		
+		System.out.print("Enter a positive integer: ");
+		int input1 = inKey.nextInt();
+		System.out.print("Enter another positive integer: ");
+		int input2 = inKey.nextInt();
+		
+		
+		
+		double result = Math.pow(input1 , input2 );
+		System.out.println(input1 +"^" +input2 + " = " + result + "\n");
 		
 	}
 	
@@ -25,6 +35,18 @@ public class Lab01 {
 	
 	
 	public static void problem02() {
+		
+		
+		Scanner inKey = new Scanner(System.in);
+		
+		System.out.print("Enter a positive integer: ");
+		int input = inKey.nextInt();
+		
+		double result = Math.sqrt(26);
+		System.out.println("the square root of " + input + " is " + result + "\n");
+		
+		
+		
 		
 		
 		
@@ -35,8 +57,19 @@ public class Lab01 {
 
 	public static void problem03() {
 		
+	Scanner inKey = new Scanner(System.in);
 		
+		System.out.print("Length on side A: ");
+		int input1 = inKey.nextInt();
+		System.out.print("Length on side B: ");
+		int input2 = inKey.nextInt();
 		
+		double sideA = Math.pow(input1, 2);
+		double sideB = Math.pow(input2, 2);
+		
+		double answer = sideA += sideB;
+		
+		System.out.println("Hypotenuse = " + answer);
 
 	}
 
@@ -45,6 +78,25 @@ public class Lab01 {
 	
 	public static void problem04() {
 		
+		int input = -1;
+		int max = -70;
+		int min = 70;
+	
+		while (input != 0) {
+			
+			Scanner inKey = new Scanner(System.in);
+			
+			System.out.print("Enter an integer: ");
+			int input1 = inKey.nextInt();
+			
+			max = Math.max(max, input1);
+			min = Math.min(min, input1);
+			
+			input = input1;
+		}
+		
+		System.out.println("Max = " + max);
+		System.out.println("Min = " + min);
 		
 		
 	}
